0c4b6e085a29a792db2a84cbc9b4e3ec7aa0a7c3
lsy0194 <tktmadltmddu@naver.com>
2023-10-05 16:13:40 +0900

    지도하나에 필요한 모든 정보를 객체로 그룹화

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -7,27 +7,25 @@ export default function Contact() {
 	//현재 카카오 객체를 cdn으로 가져오고 있 기떄문에
 	//리액트 컴포넌트안쪽에서 window객체로부터 kakao객체를 비구조할당을 이용해서 수동으로 꺼내옴
 	const { kakao } = window;
-	const mapOption = {
-		center: new kakao.maps.LatLng(37.58505853823841, 126.88550248166754), //지도의 중심좌표.
-		level: 1, //지도의 레벨(확대, 축소 정도)
+
+	const info = {
+		latlng: new kakao.maps.LatLng(37.58505853823841, 126.88550248166754),
+		imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+		imgSize: new kakao.maps.Size(232, 99),
+		imgPos: { offset: new kakao.maps.Point(116, 90) },
 	};
 
-	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`; // 마커이미지의 주소입니다
-	const imageSize = new kakao.maps.Size(232, 99); // 마커이미지의 크기입니다
-	const imageOption = { offset: new kakao.maps.Point(116, 90) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-	const markerPosition = mapOption.center;
-	//마커 위치 인스턴스를 인수로 전달해서 마커 출력 인스턴스 객체를 생성
 	const marker = new kakao.maps.Marker({
-		position: markerPosition,
-		image: markerImage, // 마커이미지 설정
+		position: info.latlng,
+		image: new kakao.maps.MarkerImage(info.imgSrc, info.imgSize, info.imgPos),
 	});
 
 	useEffect(() => {
 		//컴포넌트 마운트 되자마자 지도인스턴스 생성
-		const instance = new kakao.maps.Map(map.current, mapOption);
-		//마커출력 인스턴스에 지도 인스턴스결합
+		const instance = new kakao.maps.Map(map.current, {
+			center: info.latlng,
+			level: 1,
+		});
 		marker.setMap(instance);
 	}, []);
 	return (
@@ -36,4 +34,3 @@ export default function Contact() {
 		</Layout>
 	);
 }
-//
