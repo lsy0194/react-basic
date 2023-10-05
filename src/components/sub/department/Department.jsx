1f9c174c8c6acb418a092ab49d0978a15661015a
lsy0194 <tktmadltmddu@naver.com>
2023-10-05 10:43:28 +0900

    유튜브 데이터 출력,.env파일 제외

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -2,6 +2,8 @@ import Layout from '../../common/layout/Layout';
 import { useEffect, useState } from 'react';
 import './Department.scss';
 const path = process.env.PUBLIC_URL;
+//1.useEffect로 컴포넌트마운트되자마자 fetch외부데이터 가져옴
+//2.데이터가 다 받아지면 useState로 state에 담아줌
 
 export default function Department() {
 	const [Department, setDepartment] = useState([]);
--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,11 +1,39 @@
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
-//import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 
 export default function Youtube() {
+	const [Youtube, setYoutube] = useState([]);
+
+	const fetchYoutube = () => {
+		const api_key = process.env.REACT_APP_YOUTUBE_API;
+		const baseurl = 'https://www.googleapis.com/youtube/v3/playlistItems';
+		const pid = 'PLNiucQiR7LtQSLOd5hZS7199O5xS7PWPO';
+		const num = 5;
+		const resultURL = `${baseurl}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+		fetch(resultURL)
+			.then((data) => data.json())
+			.then((json) => {
+				console.log(json.items);
+				setYoutube(json.items);
+			});
+	};
+	useEffect(() => {
+		fetchYoutube();
+	}, []);
 	return (
 		<Layout title={'Youtube'}>
-			<p>유튜브 페이지입니다.</p>
+			{Youtube.map((data, idx) => {
+				return (
+					<article key={idx}>
+						<h2>{data.snippet.title}</h2>
+						<p>{data.snippet.description}</p>
+						<div className='pic'>
+							<img src={data.snippet.thumbnails.standard.url} alt={data.title} />
+						</div>
+					</article>
+				);
+			})}
 		</Layout>
 	);
 }
