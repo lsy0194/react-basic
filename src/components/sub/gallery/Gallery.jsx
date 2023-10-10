f1eca5a6280733b66e5caf008d69375585f0a470
lsy0194 <tktmadltmddu@naver.com>
2023-10-10 11:21:54 +0900

    검색 메서드 추가

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -11,6 +11,7 @@ export default function Gallery() {
 		const api_key = 'f5f709637c0790e1103971c1302b229e';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
+		const method_search = 'flickr.photos.search';
 		const num = 50;
 
 		//fetching함수 호출시 타입값이 있는 객체를 인수로 전달하면 해당 타입에 따라 호출 URL이 변경되고
@@ -21,7 +22,9 @@ export default function Gallery() {
 		if (opt.type === 'user') {
 			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&user_id=${opt.id}`;
 		}
-
+		if (opt.type === 'search') {
+			url = `https://www.flickr.com/services/rest/?method=${method_search}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&tags=${opt.tags}`;
+		}
 		const data = await fetch(url);
 		const json = await data.json();
 		console.log(json.photos.photo);
@@ -31,8 +34,10 @@ export default function Gallery() {
 	useEffect(() => {
 		//type: 'interest' 인터레스트 방식 갤러리 호출
 		//type: 'user' 사용자 아이디 계정의 갤러리 호출
-		fetchData({ type: 'user', id: my_id });
+		//type: 'search' 검색키워드로 갤러리 호출
+		//fetchData({ type: 'user', id: my_id });
 		//fetchData({ type: 'interest' });
+		fetchData({ type: 'search', tags: 'ocean' });
 	}, []);
 
 	return (
