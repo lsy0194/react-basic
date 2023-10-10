a9760d02856c52641d19f967cf645f8e1f2b7b32
lsy0194 <tktmadltmddu@naver.com>
2023-10-10 10:51:14 +0900

    data fetching함수 async await로 개선,사용자 아이디로 갤러리 추가 방식 개션

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -2,24 +2,37 @@ import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import { useRef, useEffect, useState } from 'react';
 import Masonry from 'react-masonry-component';
-
-const masonryOptions = {
-	transitionDuration: 0,
-};
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
-	const api_key = 'f5f709637c0790e1103971c1302b229e';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 500;
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+	const my_id = '199274089@N03';
+
+	const fetchData = async (opt) => {
+		let url = '';
+		const api_key = 'f5f709637c0790e1103971c1302b229e';
+		const method_interest = 'flickr.interestingness.getList';
+		const method_user = 'flickr.people.getPhotos';
+		const num = 50;
+
+		//fetching함수 호출시 타입값이 있는 객체를 인수로 전달하면 해당 타입에 따라 호출 URL이 변경되고
+		//해당URL을 통해 받아지는 데이터로 달라짐
+		if (opt.type === 'interest') {
+			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+		}
+		if (opt.type === 'user') {
+			url = `https://www.flickr.com/services/rest/?method=${method_user}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json&user_id=${opt.id}`;
+		}
+
+		const data = await fetch(url);
+		const json = await data.json();
+		console.log(json.photos.photo);
+		setPics(json.photos.photo);
+	};
 
 	useEffect(() => {
-		fetch(url)
-			.then((data) => data.json())
-			.then((json) => {
-				console.log(json.photos.photo);
-				setPics(json.photos.photo);
-			});
+		//type: 'interest' 인터레스트 방식 갤러리 호출
+		//type: 'user' 사용자 아이디 계정의 갤러리 호출
+		fetchData({ type: 'user', id: my_id });
+		//fetchData({ type: 'interest' });
 	}, []);
 
 	return (
