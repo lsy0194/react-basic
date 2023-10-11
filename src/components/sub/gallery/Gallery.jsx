b25b852a444975ea287811eab288a02b4ef7b853
lsy0194 <tktmadltmddu@naver.com>
2023-10-11 09:39:54 +0900

    이벤트 핸들러 함수 분리

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -21,12 +21,11 @@ export default function Gallery() {
 		setLoader(true);
 		refFrame.current.classList.remove('on');
 		let url = '';
-		const api_key = 'f5f709637c0790e1103971c1302b229e';
+		const api_key = '2a1a0aebb34012a99c23e13b49175343';
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
 		const method_search = 'flickr.photos.search';
-		const num = 10;
-
+		const num = 50;
 		if (opt.type === 'interest') {
 			url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
 		}
@@ -38,21 +37,17 @@ export default function Gallery() {
 		}
 		const data = await fetch(url);
 		const json = await data.json();
-		console.log(json.photos.photo);
 		if (json.photos.photo.length === 0) {
 			return alert('해당 검색어의 결과값이 없습니다.');
 		}
-
 		setPics(json.photos.photo);
+
 		const imgs = refFrame.current?.querySelectorAll('img');
-		console.log(imgs);
 
 		imgs.forEach((img) => {
 			img.onload = () => {
 				++count;
-				console.log('현재 로딩된 img갯수', count);
 				if (count === (Fix ? imgs.length / 2 - 1 : imgs.length - 2)) {
-					console.log('모든 이미지 소스 렌더링 완료!');
 					setLoader(false);
 					refFrame.current.classList.add('on');
 				}
@@ -60,12 +55,54 @@ export default function Gallery() {
 		});
 	};
 
+	//submit이벤트 발생시 실행할 함수
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setIsUser(false);
+
+		const btns = refBtnSet.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+
+		if (refInput.current.value.trim() === '') {
+			return alert('검색어를 입력하세요.');
+		}
+
+		fetchData({ type: 'search', tags: refInput.current.value });
+		refInput.current.value = '';
+	};
+
+	//myGallery 클릭 이벤트 발생시 실행할 함수
+	const handleClickMy = (e) => {
+		setIsUser(true);
+		if (e.target.classList.contains('on')) return;
+
+		const btns = refBtnSet.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e.target.classList.add('on');
+
+		fetchData({ type: 'user', id: my_id });
+	};
+
+	//Interest Gallery 클릭 이벤트 발생시 실행할 함수
+	const handleClickInterest = (e) => {
+		setIsUser(false);
+		if (e.target.classList.contains('on')) return;
+
+		const btns = refBtnSet.current.querySelectorAll('button');
+		btns.forEach((btn) => btn.classList.remove('on'));
+		e.target.classList.add('on');
+
+		fetchData({ type: 'interest' });
+	};
+
+	//profile 아이디 클릭시 실행할 함수
+	const handleClickProfile = (e) => {
+		if (IsUser) return;
+		fetchData({ type: 'user', id: e.target.innerText });
+		setIsUser(true);
+	};
+
 	useEffect(() => {
-		//type: 'interest' 인터레스트 방식 갤러리 호출
-		//type: 'user' 사용자 아이디 계정의 갤러리 호출
-		//type: 'search' 검색키워드로 갤러리 호출
-		//fetchData({ type: 'user', id: my_id });
-		//fetchData({ type: 'interest' });
 		fetchData({ type: 'user', id: my_id });
 	}, []);
 
@@ -73,55 +110,17 @@ export default function Gallery() {
 		<>
 			<Layout title={'Gallery'}>
 				<div className='searchBox'>
-					<form
-						onSubmit={(e) => {
-							e.preventDefault();
-							setIsUser(false);
-							const btns = refBtnSet.current.querySelectorAll('button');
-							btns.forEach((btn) => btn.classList.remove('on'));
-							if (refInput.current.value.trim() === '') {
-								return alert('검색어를 입력하세요.');
-							}
-
-							fetchData({ type: 'search', tags: refInput.current.value });
-							refInput.current.value = '';
-						}}
-					>
+					<form onSubmit={handleSubmit}>
 						<input ref={refInput} type='text' placeholder='검색어를 입력하세요' />
 						<button>검색</button>
 					</form>
 				</div>
 
 				<div className='btnSet' ref={refBtnSet}>
-					<button
-						className='on'
-						onClick={(e) => {
-							setIsUser(true);
-							if (e.target.classList.contains('on')) return;
-
-							const btns = refBtnSet.current.querySelectorAll('button');
-							btns.forEach((btn) => btn.classList.remove('on'));
-							e.target.classList.add('on');
-
-							fetchData({ type: 'user', id: my_id });
-						}}
-					>
+					<button className='on' onClick={handleClickMy}>
 						My Gallery
 					</button>
-					<button
-						onClick={(e) => {
-							setIsUser(false);
-							if (e.target.classList.contains('on')) return;
-
-							const btns = refBtnSet.current.querySelectorAll('button');
-							btns.forEach((btn) => btn.classList.remove('on'));
-							e.target.classList.add('on');
-
-							fetchData({ type: 'interest' });
-						}}
-					>
-						Interest Gallery
-					</button>
+					<button onClick={handleClickInterest}>Interest Gallery</button>
 				</div>
 
 				{Loader && (
@@ -165,15 +164,7 @@ export default function Gallery() {
 													);
 												}}
 											/>
-											<span
-												onClick={() => {
-													if (IsUser) return;
-													fetchData({ type: 'user', id: data.owner });
-													setIsUser(true);
-												}}
-											>
-												{data.owner}
-											</span>
+											<span onClick={handleClickProfile}>{data.owner}</span>
 										</div>
 									</div>
 								</article>
