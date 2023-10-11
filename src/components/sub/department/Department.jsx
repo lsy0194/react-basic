435f92d066e4d1d91afadfc05caade17b6696629
lsy0194 <tktmadltmddu@naver.com>
2023-10-11 15:33:06 +0900

    department 오른쪽 비대칭 영역 큰 레이아웃 스타일링

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -24,26 +24,30 @@ export default function Department() {
 	return (
 		//레이아웃에 타이틀을 Department로 넘김
 		<Layout title={'Department'}>
-			<div className='memberBox'>
-				{
-					//setDepartment에 저장된 값을 Department로 불러와서 .map을 통해서 차례대로 보냄
-					//idx는 순서,member는 members 배열
-					Department.map((member, idx) => {
-						return (
-							<article key={idx}>
-								<div className='pic'>
-									{/*
-										``으로 열어서 ${}으로 path에 저장된 public폴더의 위치를 불러오고
-										img폴더안에 있는 이미지중 member.pic에 저장되어있던 이미지를 불러옴
-									 */}
-									<img src={`${path}/img/${member.pic}`} alt={member.name} />
-								</div>
-								<h2>{member.name}</h2>
-								<p>{member.position}</p>
-							</article>
-						);
-					})
-				}
+			<div className='container'>
+				<div className='infobox'></div>
+
+				<div className='memberBox'>
+					{
+						//setDepartment에 저장된 값을 Department로 불러와서 .map을 통해서 차례대로 보냄
+						//idx는 순서,member는 members 배열
+						Department.map((member, idx) => {
+							return (
+								<article key={idx}>
+									<div className='pic'>
+										{/*
+											``으로 열어서 ${}으로 path에 저장된 public폴더의 위치를 불러오고
+											img폴더안에 있는 이미지중 member.pic에 저장되어있던 이미지를 불러옴
+										 */}
+										<img src={`${path}/img/${member.pic}`} alt={member.name} />
+									</div>
+									<h2>{member.name}</h2>
+									<p>{member.position}</p>
+								</article>
+							);
+						})
+					}
+				</div>
 			</div>
 		</Layout>
 	);
