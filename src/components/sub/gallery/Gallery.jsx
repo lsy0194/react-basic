1fcdbb69c2110de1fc53971011ffb6ba0a9d2df7
lsy0194 <tktmadltmddu@naver.com>
2023-10-11 10:21:35 +0900

    gallery 모달 창 닫기

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -15,7 +15,7 @@ export default function Gallery() {
 	const [IsUser, setIsUser] = useState(true);
 	const [ActiveURL, setActiveURL] = useState('');
 	const [Open, setOpen] = useState(false);
-
+	const [IsModal, setIsModal] = useState(false);
 	const fetchData = async (opt) => {
 		let count = 0;
 		setLoader(true);
@@ -147,7 +147,7 @@ export default function Gallery() {
 											alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
 											onClick={(e) => {
 												setActiveURL(e.target.getAttribute('alt'));
-												setOpen(true);
+												setIsModal(true);
 											}}
 										/>
 										<h2>{data.title}</h2>
@@ -174,8 +174,8 @@ export default function Gallery() {
 				</div>
 			</Layout>
 
-			{Open && (
-				<Modal>
+			{IsModal && (
+				<Modal setIsModal={setIsModal}>
 					<img src={ActiveURL} alt='img' />
 				</Modal>
 			)}
