863f61af0878701b707b0c222e46efc96da905bc
lsy0194 <tktmadltmddu@naver.com>
2023-10-16 09:44:59 +0900

    기본 인풋요소 입력,form관련 리셋 초기화

--- src/components/sub/members/Members.jsx
+++ src/components/sub/members/Members.jsx
@@ -4,7 +4,49 @@ import './Members.scss';
 export default function Members() {
 	return (
 		<Layout title={'Members'}>
-			<p>회원가입 페이지입니다.</p>
+			<form>
+				<fieldset>
+					<legend className='h'>회원가입 폼 양식</legend>
+					<table border='1'>
+						<tbody>
+							{/* userid */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='userid'>userid</label>
+								</th>
+								<td>
+									<input type='text' id='userid' name='userid' />
+								</td>
+							</tr>
+							{/* password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pw1'>password</label>
+								</th>
+								<td>
+									<input type='password' id='pw1' name='pw1' />
+								</td>
+							</tr>
+							{/* re password */}
+							<tr>
+								<th scope='row'>
+									<label htmlFor='pw2'>re password</label>
+								</th>
+								<td>
+									<input type='password' id='pw2' name='pw2' />
+								</td>
+							</tr>
+							{/* btnSet */}
+							<tr>
+								<th colSpan='2'>
+									<input type='reset' value='cancel' />
+									<input type='submit' value='send' />
+								</th>
+							</tr>
+						</tbody>
+					</table>
+				</fieldset>
+			</form>
 		</Layout>
 	);
 }
